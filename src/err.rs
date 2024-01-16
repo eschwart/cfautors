058e442734fa3b604a896815a3def340a8b2f4b8
splurf <evandotschwartzentruber@gmail.com>
2024-01-16 13:52:32 -0500

    compat for parsing

--- src/err.rs
+++ src/err.rs
@@ -5,11 +5,25 @@ pub enum Error {
     Cloudflare,
     API,
     Invalid,
+    IO(std::io::Error),
+    ParseInt(std::num::ParseIntError),
 }
 
 impl From<reqwest::Error> for Error {
     fn from(value: reqwest::Error) -> Self {
         Self::Reqwest(value)
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(value: std::io::Error) -> Self {
+        Self::IO(value)
+    }
+}
+
+impl From<std::num::ParseIntError> for Error {
+    fn from(value: std::num::ParseIntError) -> Self {
+        Self::ParseInt(value)
     }
 }
 
@@ -26,6 +40,10 @@ impl std::fmt::Display for Error {
             Self::Cloudflare => "Failed to properly retrieve dns record".to_string(),
             Self::API => "All public ip API failed".to_string(),
             Self::Invalid => "Invalid parsing for DNS record name".to_string(),
+            Self::IO(e) => e.to_string(),
+            Self::ParseInt(e) => e.to_string(),
         })
     }
 }
+
+impl std::error::Error for Error {}
